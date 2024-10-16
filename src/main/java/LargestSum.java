a867d165ebc450ebc42cafdbe6efe4aba3a574e2
Daniel Olen <danolen89@gmail.com>
2024-10-16 17:45:39 +0000

    Coding Challenge: Largest Sum Completed

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -10,6 +10,18 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        int largestSum = nums.get(0) + nums.get(1);
+
+    
+    for (int i = 0; i < nums.size() - 1; i++) {
+        
+        for (int j = i + 1; j < nums.size(); j++) {
+           
+            if (nums.get(i) + nums.get(j) > largestSum) {
+                largestSum = nums.get(i) + nums.get(j);
+            }
+        }
     }
+    return largestSum;
+}
 }
